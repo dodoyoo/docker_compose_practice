7774020336c20215eba1149b641b72b804d87989
dodoyoo <dong15903@daum.net>
2024-12-09 14:14:42 +0900

    초기세팅

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const { AppDataSource } = require('./src/models/data_source');
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('combined'));
+app.use(express.json());
+
+app.get('/ping', (req, res, next) => {
+  res.status(200).json({ message: 'pong' });
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, async () => {
+  await AppDataSource.initialize()
+    .then(() => {
+      console.log('Data Source hs been initialized!');
+    })
+    .catch((error) => {
+      console.error('Error during Data Source initialization', error);
+    });
+  console.log(`Listening to request on port: ${PORT}`);
+});
